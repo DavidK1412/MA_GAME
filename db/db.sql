f4499fdeb86f6f6d2e764076f70efc3301fb01f4
DavidK1412 <davidcasallas013@gmail.com>
2025-06-03 12:35:40 -0500

    finish

--- db/db.sql
+++ db/db.sql
@@ -6,18 +6,45 @@ CREATE TABLE difficulty (
 
 CREATE TABLE game (
     id VARCHAR(36) PRIMARY KEY,
-    difficulty_id INT NOT NULL,
-    created_at DATE DEFAULT CURRENT_DATE,
-    init_time TIME(0) DEFAULT CURRENT_TIME,
-    FOREIGN KEY (difficulty_id) REFERENCES difficulty(id)
+    is_finished BOOLEAN NOT NULL DEFAULT FALSE,
+    buclicity_avg FLOAT DEFAULT 0,
+    branch_factor_avg FLOAT DEFAULT 0,
 );
+
+CREATE TABLE game_attempts (
+    id VARCHAR(36) PRIMARY KEY,
+    game_id VARCHAR(36) NOT NULL,
+    difficulty_id INT NOT NULL,
+    last_buclicity FLOAT NOT NULL DEFAULT 0,
+    last_branch_factor FLOAT NOT NULL DEFAULT 0,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (difficulty_id) REFERENCES difficulty(id)
+)
 
 CREATE TABLE movements (
     id VARCHAR(36) PRIMARY KEY,
-    game_id VARCHAR(36) NOT NULL,
+    attempt_id VARCHAR(36) NOT NULL,
     movement_time TIME(0) DEFAULT CURRENT_TIME,
     step INT NOT NULL,
-    movement VARCHAR(13)
+    movement VARCHAR(20),
+    is_correct BOOLEAN NOT NULL DEFAULT FALSE,
+    interuption BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (attempt_id) REFERENCES game_attempts(id)
 );
 
+CREATE TABLE movements_misses (
+    id VARCHAR(36) PRIMARY KEY,
+    game_attempt_id VARCHAR(36) NOT NULL,
+    count INT NOT NULL,
+    FOREIGN KEY (game_attempt_id) REFERENCES game_attempts(id)
+)
+
+-- [_, _, _, _, _, _, _] easy
+-- [_, _, _, _, _, _, _, _, _] medium
+-- [_, _, _, _, _, _, _, _, _, _, _] hard
+-- [10, 9, 8, 7, 6, 0, 1, 2, 3, 4, 5]
+
 INSERT INTO difficulty (name, number_of_blocks) VALUES ('easy', 7);
+INSERT INTO difficulty (name, number_of_blocks) VALUES ('medium', 9);
+INSERT INTO difficulty (name, number_of_blocks) VALUES ('hard', 12);
