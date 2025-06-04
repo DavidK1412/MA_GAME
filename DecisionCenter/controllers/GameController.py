24ee6a722dd8a5e0189b5d5cf57c21a4eda206e4
DavidK1412 <davidcasallas013@gmail.com>
2025-06-04 12:09:01 -0500

    fix

--- DecisionCenter/controllers/GameController.py
+++ DecisionCenter/controllers/GameController.py
@@ -66,7 +66,7 @@ class GameController:
             difficulty = {
                 1: {
                     "blocks_per_team": 3,
-                    "final_state": [6, 5, 4, 0, 1, 2, 3]
+                    "final_state": [4, 5, 6, 0, 1, 2, 3]
                 },
                 2: {
                     "blocks_per_team": 4,
